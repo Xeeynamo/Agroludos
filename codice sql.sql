52746b5070f101756a2bf92b7633739190a90400
Ros290 <redrum290@gmail.com>
2014-07-11 12:57:41 +0200

    tutte le tabelle implementate

--- codice sql.sql
+++ codice sql.sql
@@ -23,3 +23,35 @@ primary key (nome)
 
 alter table mc add foreign key (mail) references utente(mail);
 alter table ms add foreign key (mail) references utente(mail);
+
+create table opt_comp
+(
+opt varchar (255) NOT NULL,
+comp int (4) NOT NULL,
+primary key (opt,comp)
+);
+
+alter table opt_comp add foreign key (opt) references optional(nome);
+
+create table competizione
+(
+id int (4) NOT NULL,
+prezzo float (5,2) NOT NULL,
+nmin int (2) NOT NULL,
+nmax int (3) NOT NULL,
+tipo varchar (255) NOT NULL,
+primary key (id)
+);
+
+
+alter table opt_comp add foreign key (comp) references competizione(id);
+
+create table prenotazione
+(
+part char (16) NOT NULL,
+comp int (4) NOT NULL,
+primary key (part,comp)
+);
+
+alter table prenotazione add foreign key (part) references partecipante(codfisc);
+alter table prenotazione add foreign key (comp) references competizione(id);
