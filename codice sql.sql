fb3d7f72fd0d340279f0dfcd17a09051ae4d84ff
Xeeynamo <xeeynamo@hotmail.com>
2014-07-19 16:47:56 +0200

    Non funge. Halp.

--- codice sql.sql
+++ codice sql.sql
@@ -1,83 +1,137 @@
-create table mc
-(
-mail varchar (255) NOT NULL,
-nome varchar (45) NOT NULL,
-cognome varchar (45) NOT NULL,
-id int (2) NOT NULL,
-primary key (id)
-);
+CREATE TABLE IF NOT EXISTS `agroludos`.`Utente` (
+  `mail` VARCHAR(255) NOT NULL,
+  `password` BLOB NOT NULL,
+  `tipo` INT NOT NULL,
+  PRIMARY KEY (`mail`),
+  UNIQUE INDEX `mail_UNIQUE` (`mail` ASC))
+ENGINE = InnoDB;
 
-create table ms
-(
-mail varchar (255) NOT NULL,
-id int (2) NOT NULL,
-primary key (id)
-);
+CREATE TABLE IF NOT EXISTS `agroludos`.`Manager` (
+  `mail` VARCHAR(255) NOT NULL,
+  `nome` VARCHAR(45) NOT NULL,
+  `cognome` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`mail`),
+  CONSTRAINT `mail`
+    FOREIGN KEY (`mail`)
+    REFERENCES `agroludos`.`Utente` (`mail`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-create table tipo_comp
-(
-nome varchar (255) NOT NULL,
-descrizione longtext,
-primary key (nome)
-);
+CREATE TABLE IF NOT EXISTS `agroludos`.`TipoCompetizione` (
+  `nome` VARCHAR(45) NOT NULL,
+  `descrizione` VARCHAR(255) NULL,
+  PRIMARY KEY (`nome`))
+ENGINE = InnoDB;
 
-alter table mc add foreign key (mail) references utente(mail);
-alter table ms add foreign key (mail) references utente(mail);
+CREATE TABLE IF NOT EXISTS `agroludos`.`Competizione` (
+  `idCompetizione` INT NOT NULL AUTO_INCREMENT,
+  `tipo` VARCHAR(45) NOT NULL,
+  `manager` VARCHAR(255) NOT NULL,
+  `data` DATE NOT NULL,
+  `partMin` INT NOT NULL,
+  `partMax` INT NOT NULL,
+  `prezzo` FLOAT(5,2) NOT NULL,
+  `annullata` TINYINT(1) NULL,
+  PRIMARY KEY (`idCompetizione`),
+  INDEX `tipo_idx` (`tipo` ASC),
+  INDEX `manager_idx` (`manager` ASC),
+  CONSTRAINT `tipo`
+    FOREIGN KEY (`tipo`)
+    REFERENCES `agroludos`.`TipoCompetizione` (`nome`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `manager`
+    FOREIGN KEY (`manager`)
+    REFERENCES `agroludos`.`Manager` (`mail`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-create table opt_comp
-(
-opt varchar (255) NOT NULL,
-comp int (4) NOT NULL,
-primary key (opt,comp)
-);
+CREATE TABLE IF NOT EXISTS `agroludos`.`Partecipante` (
+  `email` VARCHAR(255) NOT NULL,
+  `nome` VARCHAR(45) NOT NULL,
+  `cognome` VARCHAR(45) NOT NULL,
+  `indirizzo` VARCHAR(255) NOT NULL,
+  `datanascita` DATE NOT NULL,
+  `codicefiscale` CHAR(16) NOT NULL,
+  `sesso` CHAR NOT NULL,
+  `tesserasan` CHAR(20) NOT NULL,
+  `datasrc` DATE NOT NULL,
+  `src` LONGTEXT BINARY NOT NULL,
+  UNIQUE INDEX `codicefiscale_UNIQUE` (`codicefiscale` ASC),
+  PRIMARY KEY (`codicefiscale`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC),
+  CONSTRAINT `email`
+    FOREIGN KEY (`email`)
+    REFERENCES `agroludos`.`Utente` (`mail`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-alter table opt_comp add foreign key (opt) references optional(nome);
+CREATE TABLE IF NOT EXISTS `agroludos`.`Prenotazione` (
+  `idPrenotazione` INT NOT NULL AUTO_INCREMENT,
+  `partecipante` VARCHAR(255) NOT NULL,
+  `competizione` INT NOT NULL,
+  PRIMARY KEY (`idPrenotazione`),
+  INDEX `partecipante_idx` (`partecipante` ASC),
+  INDEX `competizione_idx` (`competizione` ASC),
+  CONSTRAINT `partecipante`
+    FOREIGN KEY (`partecipante`)
+    REFERENCES `agroludos`.`Partecipante` (`email`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `competizione`
+    FOREIGN KEY (`competizione`)
+    REFERENCES `agroludos`.`Competizione` (`idCompetizione`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-create table competizione
-(
-id int (4) NOT NULL,
-prezzo float (5,2) NOT NULL,
-nmin int (2) NOT NULL,
-nmax int (3) NOT NULL,
-tipo varchar (255) NOT NULL,
-primary key (id)
-);
+CREATE TABLE IF NOT EXISTS `agroludos`.`Optional` (
+  `nome` VARCHAR(45) NOT NULL,
+  `descrizione` LONGTEXT NULL,
+  `prezzo` FLOAT(5,2) NULL,
+  PRIMARY KEY (`nome`))
+ENGINE = InnoDB;
 
-alter table competizione add foreign key (tipo) references tipo_comp(nome);
+CREATE TABLE IF NOT EXISTS `agroludos`.`Optional_Competizione` (
+  `idOptionalCompetizione` INT NOT NULL AUTO_INCREMENT,
+  `optional` VARCHAR(45) NOT NULL,
+  `competizione` INT NOT NULL,
+  `prezzo` FLOAT(5,2) NOT NULL,
+  INDEX `optional_idx` (`optional` ASC),
+  INDEX `competizione_idx` (`competizione` ASC),
+  PRIMARY KEY (`optional`, `competizione`),
+  UNIQUE INDEX `idOptionalCompetizione` (`idOptionalCompetizione` ASC),
+  CONSTRAINT `optional`
+    FOREIGN KEY (`optional`)
+    REFERENCES `agroludos`.`Optional` (`nome`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `competizione`
+    FOREIGN KEY (`competizione`)
+    REFERENCES `agroludos`.`Competizione` (`idCompetizione`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-alter table opt_comp add foreign key (comp) references competizione(id);
-
-create table prenotazione
-(
-part char (16) NOT NULL,
-comp int (4) NOT NULL,
-primary key (part,comp)
-);
-
-alter table prenotazione add foreign key (part) references partecipante(codfisc);
-alter table prenotazione add foreign key (comp) references competizione(id);
-
-alter table competizione add manager_comp int (2) NOT NULL;
-
-alter table competizione add foreign key (manager_comp) references mc(id);
-
-alter table opt_comp add prezzo float (4,2) NOT NULL;
-
-create table opt_pren
-(
-opt varchar (255) NOT NULL,
-part char(16) NOT NULL,
-comp int (4) NOT NULL,
-primary key (opt,part,comp)
-);
-
-alter table opt_pren add foreign key (opt,comp) references opt_comp(opt,comp);
-alter table opt_pren add foreign key (part,comp) references prenotazione (part,comp);
- 
-alter table partecipante drop src;
-
-alter table partecipante add src varchar (255) NOT NULL;
-
-alter table competizione add data_comp date NOT NULL;
+CREATE TABLE IF NOT EXISTS `agroludos`.`Optional_Prenotazione` (
+  `optional` INT NULL,
+  `prenotazione` INT NULL,
+  `selezionato` TINYINT(1) NULL,
+  INDEX `optional_idx` (`optional` ASC),
+  INDEX `prenotazione_idx` (`prenotazione` ASC),
+  CONSTRAINT `optional`
+    FOREIGN KEY (`optional`)
+    REFERENCES `agroludos`.`Optional_Competizione` (`idOptionalCompetizione`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `prenotazione`
+    FOREIGN KEY (`prenotazione`)
+    REFERENCES `agroludos`.`Prenotazione` (`idPrenotazione`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
