ab14c0353adfdb2f7b5095a2c2c4637c4e58e45a
Xeeynamo <xeeynamo@hotmail.com>
2014-07-21 02:35:18 +0200

    AgroMail funzionante con un account creato al volo

--- AGROLUDOS/src/agroludos/db/AgroMail.java
+++ AGROLUDOS/src/agroludos/db/AgroMail.java
@@ -8,15 +8,17 @@ import javax.mail.internet.*;
 public class AgroMail
 {
     private static final String CHOSEN_SERVER_ADDRESS = "smtp.gmail.com";
-    private static final int CHOSEN_SERVER_PORT = 465;
+    private static final int CHOSEN_SERVER_PORT = 587;
+    private static final String CHOSEN_USERNAME = "agroludos.ingegneria@gmail.com";
+    private static final String CHOSEN_PASSWORD = "agroludosL&L";
     
     String from, to, subject, text;
     
     public AgroMail(String from, String to, String subject, String text)
     {
-        this.from = from;
+        this.from = CHOSEN_USERNAME;
         this.to = to;
-        this.subject = subject;
+        this.subject = subject + " (" + from + ")";
         this.text = text;
     }
     
@@ -25,6 +27,9 @@ public class AgroMail
         Properties p = new Properties();
         p.put("mail.smtp.host", CHOSEN_SERVER_ADDRESS);
         p.put("mail.smpt.port", String.valueOf(CHOSEN_SERVER_PORT));
+        p.put("mail.smtp.starttls.enable", "true");
+        p.put("mail.smtp.auth", "true");
+        p.put("mail.smtp.socketFactory.fallback", "false");
         
         Session mailSession = Session.getDefaultInstance(p);
         Message message = new MimeMessage(mailSession);
@@ -36,5 +41,10 @@ public class AgroMail
         message.setRecipient(RecipientType.TO, toAddress);
         message.setSubject(subject);
         message.setText(text);
+        
+        Transport transport = mailSession.getTransport("smtp");
+        transport.connect(CHOSEN_SERVER_ADDRESS, from, CHOSEN_PASSWORD);
+        transport.sendMessage(message, message.getAllRecipients());
+        transport.close();
     }
 }
