0f219295ffe349eed8a1f2b41cd46528a95939b9
Xeeynamo <xeeynamo@hotmail.com>
2014-09-21 13:14:44 +0200

    Dispatcher completato!

--- AGROLUDOS/src/agroludos/server/Dispatcher.java
+++ AGROLUDOS/src/agroludos/server/Dispatcher.java
@@ -18,6 +18,68 @@ import agroludos.server.exception.RequestNotSupportedException;
  */
 public class Dispatcher
 {
+    static class Pair
+    {
+        String name;
+        Request request;
+        public Pair(String name, Request request)
+        {
+            this.name = name;
+            this.request = request;
+        }
+        String getName()
+        {
+            return name;
+        }
+        Request getRequest()
+        {
+            return request;
+        }
+    }
+    private static final Pair[] pair =
+    {
+        new Pair("Initialize", Request.Initialize),
+        new Pair("Login", Request.Login),
+        new Pair("AddPartecipante", Request.AddPartecipante),
+        new Pair("SendMail", Request.SendMail),
+        new Pair("SendMailToSys", Request.SendMailToSys),
+        new Pair("GetManagers", Request.GetManagers),
+        new Pair("GetCompetizioniDisponibili", Request.GetCompetizioniDisponibili),
+        new Pair("GetCompetizioniPrenotate", Request.GetCompetizioniPrenotate),
+        new Pair("GetCompetizione", Request.GetCompetizione),
+        new Pair("GetCompetizioneFromId", Request.GetCompetizioneFromId),
+        new Pair("GetCompetizioni", Request.GetCompetizioni),
+        new Pair("GetCompetizioniMinimal", Request.GetCompetizioniMinimal),
+        new Pair("AnnullaPrenotazione", Request.AnnullaPrenotazione),
+        new Pair("AddIscrizioneCompetizione", Request.AddIscrizioneCompetizione),
+        new Pair("IsOptionalSelezionato", Request.IsOptionalSelezionato),
+        new Pair("SetOptionalPrenotazione", Request.SetOptionalPrenotazione),
+        new Pair("GetOptional", Request.GetOptional),
+        new Pair("SetOptional", Request.SetOptional),
+        new Pair("SetOptionalPrenotazione", Request.SetOptionalPrenotazione),
+        new Pair("GetPartecipante", Request.GetPartecipante),
+        new Pair("GetPartecipanti", Request.GetPartecipanti),
+        new Pair("GetPartecipantiCompetizione", Request.GetPartecipantiCompetizione),
+        new Pair("GetPartecipantiMinimal", Request.GetPartecipantiMinimal),
+        new Pair("GetPartecipanteCompetizioni", Request.GetPartecipanteCompetizioni),
+        new Pair("GetCompetizioneTipi", Request.GetCompetizioneTipi),
+        new Pair("AnnullaCompetizione", Request.AnnullaCompetizione),
+        new Pair("AddCompetizione", Request.AddCompetizione),
+        new Pair("IsModificaScaduto", Request.isModificaScaduto),
+        new Pair("SetNPartMax", Request.setNPartMax),
+        new Pair("SetNPartMin", Request.setNPartMin),
+        new Pair("SetPrezzoComp", Request.setPrezzoComp),
+        new Pair("SetOptionalCompetizione", Request.setOptionalCompetizione),
+        new Pair("DropOptionalCompetizione", Request.dropOptionalCompetizione),
+        new Pair("GetOptional", Request.getOptional),
+        new Pair("FrameLogin", Request.FrameLogin),
+        new Pair("FrameRegistrazione", Request.FrameRegistrazione),
+        new Pair("FrameHome", Request.FrameHome),
+        new Pair("FrameManagerCompetizione", Request.FrameManagerCompetizione),
+        new Pair("FrameCreaCompetizione", Request.FrameCreaCompetizione),
+        new Pair("FrameManagerSistema", Request.FrameManagerSistema),
+    };
+    
     private final ApplicationController appCtrl;
     public Dispatcher()
     {
@@ -27,6 +89,11 @@ public class Dispatcher
             throws DeniedRequestException, RequestNotSupportedException, InternalErrorException
     {
         Request r = null;
-        return appCtrl.processRequest(r, parameter);
+        for (int i = 0; i < pair.length; i++)
+        {
+            if (request.compareTo(pair[i].getName()) == 0)
+                return appCtrl.processRequest(pair[i].getRequest(), parameter);
+        }
+        return null;
     }
 }
